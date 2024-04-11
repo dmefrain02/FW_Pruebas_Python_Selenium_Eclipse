d5053c398e85e8ccd54d26b8890000ad4fb1f607
dmefrain02 <dmefrain.02@gmail.com>
2024-04-10 21:28:29 -0600

    cambios webdriver manager y otros cambios
    cambios webdriver manager y otros cambios

--- EnviromentQA/src/Test/test3.bat
+++
@@ -1,8 +0,0 @@
-echo ## Ir a la carpeta del archivo ##
-cd "C:\Users\dmefr\Desktop\Carpeta MEGA\Videos QA\Curso Automatizacion Pruebas\Framework Pruebas Automatizadas\Entornos Virtuales\EnviromentQA\Framework Automatizacion Pruebas\src\Test"
-
-echo ## Ejecucion del Archivo con Mensaje desde la Consola ##
-python test3.py
-
-echo ## Fin de la ejecucion del archivo ##
-pause
