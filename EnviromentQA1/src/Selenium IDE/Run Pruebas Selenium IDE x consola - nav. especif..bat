c67658581f4db9d610248d9239858b31a84928db
dmefrain02 <dmefrain.02@gmail.com>
2024-07-30 19:00:52 -0600

    selenium ide

--- EnviromentQA1/src/Selenium IDE/Run Pruebas Selenium IDE x consola - nav. especif..bat
+++ EnviromentQA1/src/Selenium IDE/Run Pruebas Selenium IDE x consola - nav. especif..bat
@@ -1,3 +1,3 @@
 cd C:\Users\dmefr\Desktop\FW_Pruebas_Python_Selenium\FW_Pruebas_Python_Selenium\EnviromentQA1\src\Selenium IDE
-selenium-side-runner "Selenium IDE.side" -c "browserName=firefox"
+selenium-side-runner "Selenium IDE.side" -c "browserName=firefox" 
 pause
