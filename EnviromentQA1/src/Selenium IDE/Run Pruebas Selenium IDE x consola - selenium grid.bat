2567b78e161001018f5e115cea85c762621f6308
Efrain Castañeda Delgado <dmefrain.02@gmail.com>
2024-09-21 16:06:43 -0600

    cambios

--- EnviromentQA1/src/Selenium IDE/Run Pruebas Selenium IDE x consola - selenium grid.bat
+++ EnviromentQA1/src/Selenium IDE/Run Pruebas Selenium IDE x consola - selenium grid.bat
@@ -1,3 +1,3 @@
-cd C:\Users\dmefr\Desktop\FW_Pruebas_Python_Selenium\FW_Pruebas_Python_Selenium_Eclipse\EnviromentQA1\src\Selenium IDE
-selenium-side-runner "Selenium IDE.side" --server http://localhost:4444/wd/hub -c "browserName='chrome' platform='Windows'"
+cd C:\Users\dmefr\OneDrive\Escritorio\FW_Pruebas_Python_Selenium_Eclipse\EnviromentQA1\src\Selenium IDE
+selenium-side-runner "Pruebas.side" --server http://localhost:4444/wd/hub -c "browserName='chrome' platform='Windows'"
 pause
