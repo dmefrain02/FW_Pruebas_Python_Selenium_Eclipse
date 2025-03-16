fba8aafa613dcd89ae18489f20fa21c3415e26cc
Efrain Castañeda Delgado <dmefrain.02@gmail.com>
2025-03-16 17:30:24 -0600

    cambios

--- EnviromentQA2/src/Test/Test47.py
+++ EnviromentQA2/src/Test/Test47.py
@@ -8,9 +8,9 @@ class Test(unittest.TestCase):
 
     def setUp(self):
         Selenium.obtener_archivo_json(self, 'Localizadores_Spotify')
-        Selenium.inicializar(self)
+        #Selenium.inicializar(self)
         
-    def Test_01(self):
+    def test_01(self):
         for Nav_Sel_Grid in Inicializar.Navegadores_Sel_Grid:
             Selenium.abrir_navegador(self,Nav_Sel_Grid, True)
             Selenium.get_url_driver(self,"https://demoqa.com/alerts")
@@ -20,21 +20,21 @@ class Test(unittest.TestCase):
             Selenium.WebdriverWait(self,2)
             Selenium.cerrar_driver_navegador(self)
         
-    def Test_02(self):
+    def test_02(self):
         Selenium.abrir_navegador(self,"Chrome")
         #recording_thread = threading.Thread(target=Selenium.start(self), daemon=True)
         #recording_thread.start()
         
         #while recording_thread:
-        while Selenium.start(self):
-            Selenium.get_url_driver(self,"https://demoqa.com/alerts")
-            Selenium.WebdriverWait(self,2)
-            Selenium.click_en_elemento(self, "btn-time")
-            Selenium.esperar_elemento(self, 5)
-            Selenium.alert_navegadores(self,1,"This alert appeared after 5 seconds","No se muestra el mensaje correcto")
-            #Selenium.cerrar_driver_navegador(self)
+        #while Selenium.start(self):
+        Selenium.get_url_driver(self,"https://demoqa.com/alerts")
+        Selenium.WebdriverWait(self,2)
+        Selenium.click_en_elemento(self, "btn-time")
+        Selenium.esperar_elemento(self, 5)
+        Selenium.alert_navegadores(self,1,"This alert appeared after 5 seconds","No se muestra el mensaje correcto")
+        #Selenium.cerrar_driver_navegador(self)
 
-    def Test_03(self):
+    def test_03(self):
         for Nav_Sel_Grid in Inicializar.Navegadores_Sel_Grid:
             Selenium.abrir_navegador(self,Nav_Sel_Grid, True)
             Selenium.get_url_driver(self,"https://demoqa.com/alerts")
@@ -45,7 +45,7 @@ class Test(unittest.TestCase):
             Selenium.esperar_elemento(self, 2)
             Selenium.cerrar_driver_navegador(self)
         
-    def Test_04(self):
+    def test_04(self):
         for Nav_Sel_Grid in Inicializar.Navegadores_Sel_Grid:
             Selenium.abrir_navegador(self,Nav_Sel_Grid, True)
             Selenium.get_url_driver(self,"https://demoqa.com/alerts")
@@ -56,7 +56,7 @@ class Test(unittest.TestCase):
             Selenium.WebdriverWait(self,2)
             Selenium.cerrar_driver_navegador(self)
         
-    def Test_05(self):
+    def test_05(self):
         Selenium.abrir_navegador(self,"Chrome")
         Selenium.get_url_driver(self,"https://demoqa.com/alerts")
         Selenium.WebdriverWait(self,2)
